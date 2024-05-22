26fac4529cdb763219f7ae44a7dfd7befac2f05a
Giovanni <gio.reder@gmail.com>
2024-05-22 16:27:46 +0200

    es1

---
+++ src/pratica/es1/Main.java
@@ -0,0 +1,10 @@
+package pratica.es1;
+
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rect = new Rettangolo(20,10);
+        rect.calcPerimetro();
+        Rettangolo rect2 = new Rettangolo(20,10);
+        rect2.calcArea();
+    }
+}
---
+++ src/pratica/es1/Rettangolo.java
@@ -0,0 +1,21 @@
+package pratica.es1;
+
+public class Rettangolo {
+    // ATTRIBUTI
+    private double larghezza;
+    private double altezza;
+    // COSTRUTTORI
+    public Rettangolo(double larghezza, double altezza){
+        this.larghezza=larghezza;
+        this.altezza=altezza;
+    }
+    // METODI
+    public void calcPerimetro(){
+        System.out.println("Ciao sono un rettangolo, il mio perimetro è "
+                + ((this.larghezza * 2) + (this.altezza * 2)));
+    }public void calcArea(){
+        System.out.println("Ciao sono un rettangolo, la mia area è "
+                + (this.altezza * this.larghezza) );
+    }
+
+}
